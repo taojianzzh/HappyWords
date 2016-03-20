2d3ff160261284c8b8e762adbde4f49372c249e9
Jared Tao <jian.tao@autodesk.com>
2016-03-20 12:53:27 +0800

    - highlight keyword

--- Source/Web/Scripts/Site/WordList.tsx
+++ Source/Web/Scripts/Site/WordList.tsx
@@ -8,11 +8,22 @@ class WordList extends React.Component<WordListProps, any> {
     render() {
         var items = [];
         var keyword = this.props.keyword;
-        this.props.words.forEach((w) => {
-            if (w.spelling.indexOf(keyword) !== -1) {
+        if (keyword) {
+            this.props.words.forEach((w) => {
+                var startIndex = w.spelling.indexOf(keyword);
+                if (startIndex >= 0) {
+                    var highlight = {
+                        start: startIndex,
+                        end: startIndex + this.props.keyword.length
+                    };
+                    items.push(<WordItem word={w} key={w.spelling} highlight={highlight} />);
+                }
+            });
+        } else {
+            this.props.words.forEach((w) => {
                 items.push(<WordItem word={w} key={w.spelling} />);
-            }
-        });
+            });
+        }
 
         return (
             <div id="word_list" className="clearfix">
