c50dd328ff9bd7e2670a95c915b662dab7e2035d
Jian Tao <taojian@live.com>
2016-12-28 16:20:12 +0800

    Authentication

---
+++ Source/Data/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HappyWords.Data.Models
+{
+    public class User : IMongoObject
+    {
+        public ObjectId Id { get; set; }
+        public string UserName { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
